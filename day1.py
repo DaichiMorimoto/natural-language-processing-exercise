437777414f9f1c148969efe6c5037e65041c307c
DaichiMorimoto <fb0120earth@gmail.com>
2019-07-04 14:54:10 +0900

    Complete System

--- day1.py
+++ day1.py
@@ -5,6 +5,8 @@ import nltk
 from nltk.corpus import wordnet as wn
 
 # 前処理
+en_stop = nltk.corpus.stopwords.words('english')
+
 def preprocessing_text(text):
     def cleaning_text(text):
         pattern1 = '@|%'
@@ -15,6 +17,8 @@ def preprocessing_text(text):
         text = re.sub(pattern3, '', text)
         pattern4 = '[0-9]'
         text = re.sub(pattern4, '', text)
+        pattern5 = '\(.*?\)'
+        text = re.sub(pattern5, '', text)
         return text
 
     def tokenize_text(text):
@@ -30,6 +34,12 @@ def preprocessing_text(text):
             return word
         else:
             return lemma
+
+    def remove_stopwords(word, stopwordset):
+        if word in stopwordset:
+            return None
+        else:
+            return word
 
     text = cleaning_text(text)
     tokens = tokenize_text(text)
@@ -59,7 +69,7 @@ def tfidf_vectorizer(docs):
             if w not in word2id:
                 word2id[w] = len(word2id)
 
-    return [[_tf*_idf for _tf, _idf in zip(tf(word2id, doc), idf(word2id, docs))] for doc in docs], word2id
+    return np.array([[_tf*_idf for _tf, _idf in zip(tf(word2id, doc), idf(word2id, docs))] for doc in docs]), word2id
 
 def bow_vectorizer(docs):
     word2id = {}
@@ -75,11 +85,11 @@ def bow_vectorizer(docs):
             doc_vec[word2id[w]] += 1
         result_list.append(doc_vec)
 
-    return result_list, word2id
+    return np.array(result_list), word2id
 
 ### 類似度計算 ###
 def cosine_similarity(list_a, list_b):
-    inner_prod = np.array(list_a).dot(np.array(list_b))
+    inner_prod = list_a.dot(list_b)
     norm_a = np.linalg.norm(list_a)
     norm_b = np.linalg.norm(list_b)
     try:
@@ -88,16 +98,63 @@ def cosine_similarity(list_a, list_b):
         return 1.0
 
 def minkowski_distance(list_a, list_b, p):
-    diff_vec = np.array(list_a) - np.array(list_b)
+    diff_vec = list_a - list_b
     return np.linalg.norm(diff_vec, ord=p)
 
 # テキストを読んでくる
-def read_docs():
-    return
+def read_docs(path):
+    df = pd.read_csv(path)
+    print(df)
+    return df
+
+def calc_sim(vec, is_cos):
+    if is_cos:
+        sims = np.zeros((vec.shape[0], vec.shape[0]))
+        for i in range(vec.shape[0]):
+            for j in range(i+1, vec.shape[0]):
+                sims[i][j] = cosine_similarity(vec[i], vec[j])
+    else:
+        sims = np.full((vec.shape[0], vec.shape[0]), np.inf)
+        for i in range(vec.shape[0]):
+            for j in range(i+1, vec.shape[0]):
+                sims[i][j] = minkowski_distance(vec[i], vec[j], 2)
+
+    return sims
+
+def print_max_pair(names, vecs, is_cos):
+    if is_cos:
+        idx = np.unravel_index(np.argmax(vecs), vecs.shape)
+    else:
+        idx = np.unravel_index(np.argmin(vecs), vecs.shape)
+    print(names[idx[0]] + ' and ' + names[idx[1]])
 
 def main():
-    docs = read_docs()
+    # CSV読み込み兼前処理
+    df = read_docs('./data/lang.csv')
+    docs = df["Abstract"].values
     pp_docs = [preprocessing_text(text) for text in docs]
+    # print(pp_docs)
+
+    # TF-IDF
+    tfidf_vector, word2id = tfidf_vectorizer(pp_docs)
+    print('======= TF-IDF =======')
+    print(tfidf_vector)
+
+    # BoW
+    bow_vec, word2id = bow_vectorizer(pp_docs)
+    print('====== BoW =======')
+    print(bow_vec)
+
+    # 類似度計算
+    cos_tfidf = calc_sim(tfidf_vector, True)
+    min_tfidf = calc_sim(tfidf_vector, False)
+    cos_bow = calc_sim(bow_vec, True)
+    min_bow = calc_sim(bow_vec, False)
+
+    print_max_pair(df["Name"].values, cos_tfidf, True)
+    print_max_pair(df["Name"].values, min_tfidf, False)
+    print_max_pair(df["Name"].values, cos_bow, True)
+    print_max_pair(df["Name"].values, min_bow, False)
 
 if __name__ == "__main__":
     main()
